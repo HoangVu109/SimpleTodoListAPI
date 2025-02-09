7241a11c314001e28d8fa1c879c781d955ddce76
hoangvu109 <vuviethoang0@gmail.com>
2025-02-09 14:06:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, World!<h1>');
+});
+
+const port = 3000;
+
+app.listen(port,() => {
+    console.log(`Server listening on port ${port}`);
+});
+
