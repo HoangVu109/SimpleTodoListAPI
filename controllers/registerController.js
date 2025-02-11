0206c233681870c812b56a9ec775b273593f7d70
hoangvu109 <vuviethoang0@gmail.com>
2025-02-11 17:21:17 +0700

    register usecase completed

---
+++ controllers/registerController.js
@@ -0,0 +1,29 @@
+const Account = require('../models/accountModel')
+const bcrypt = require('bcrypt')
+
+exports.register = async function(req, res, next) {
+    try {
+        const existingAccount = await Account.findOne({ email: req.body.email });
+
+        if (existingAccount == null) {
+            bcrypt.hash(req.body.password, 10, async function(err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                const newAccount = new Account(req.body);
+                newAccount.password = hash;
+
+                try {
+                    const result = await newAccount.save();
+                    res.json({ account: result });
+                } catch (saveErr) {
+                    return res.json({ err: saveErr });
+                }
+            });
+        } else {
+            res.json({ err: 'Email đã được sử dụng' });
+        }
+    } catch (error) {
+        return res.status(500).json({ err: "Lỗi server." });
+    }
+};
--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const express = require('express');
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const routes = require('./routes/routes')
 
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!<h1>');
-});
 
-const port = 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
+const db = mongoose.connection;
 
-app.listen(port,() => {
-    console.log(`Server listening on port ${port}`);
-});
+dotenv.config()
 
+//connect db
+const uri = process.env.DB_URL
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+    try {
+      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+      await mongoose.connect(uri, clientOptions);
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log("Kết nối thành công MongoDB");
+      console.log('DB hiện tại: '+ mongoose.connection.db.databaseName)
+    } catch (e) {
+        console.log("Kết nối thất bại");
+    }
+}
+
+run().catch(console.dir);
+
+app.use(express.json());
+app.use('/',routes)
+
+
+
+
+app.listen(PORT, () => {console.log("Server started on http://localhost:"+PORT)})
+
+module.exports = app;
